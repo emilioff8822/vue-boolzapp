de5fc1dbf0da2ee1695a68617662e1bc9fb0628c
emilioff8822 <emilioff@hotmail.it>
2023-04-17 13:37:10 +0200

    comm-1-js-contacts

---
+++ milestone1/js/script.js
@@ -0,0 +1,41 @@
+const {createApp}= Vue;
+createApp ({
+
+data (){
+
+return {
+  contacts: [
+    { id: 1, name: 'Contatto 1', avatar: 'avatar_1.jpg', lastMessage: 'Ciao!' },
+    { id: 2, name: 'Contatto 2', avatar: 'avatar_2.jpg', lastMessage: 'A presto!' },
+    { id: 3, name: 'Contatto 3', avatar: 'avatar_3.jpg', lastMessage: 'Come va?' },
+    { id: 4, name: 'Contatto 4', avatar: 'avatar_4.jpg', lastMessage: 'Buona giornata!' },
+    { id: 5, name: 'Contatto 5', avatar: 'avatar_5.jpg', lastMessage: 'Ci vediamo!' },
+  ],
+  filteredContacts: [],
+  search: '',
+  activeContact: { name: '' },
+  activeContactColor: '#4caf50',
+  messages: [],
+  inputMessage: '',
+
+};  
+
+
+
+},
+
+methods: {
+
+  
+
+
+
+},
+
+mounted(){
+
+
+
+}
+
+}).mount("#app")
