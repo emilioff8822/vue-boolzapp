b7c1ead7c97ca5dcfe3234febc4ad8c00c6e73e2
emilioff8822 <emilioff@hotmail.it>
2023-04-19 11:23:04 +0200

    milestone4

---
+++ milestone4/js/script.js
@@ -0,0 +1,67 @@
+const { createApp } = Vue;
+const dt = luxon.DateTime
+
+createApp({
+  data() {
+    return {
+      contacts: [
+      { id: 1, name: 'Ulisse', avatar: 'avatar_1.jpg', lastMessage: '', lastMessageDate: '2023-04-17T10:30:00', messages: [] },
+      { id: 2, name: 'Emilio', avatar: 'avatar_2.jpg', lastMessage: '', lastMessageDate: '2023-04-16T15:45:00', messages: [] },
+      { id: 3, name: 'Icaro', avatar: 'avatar_3.jpg', lastMessage: '', lastMessageDate: '2023-04-15T12:00:00', messages: [] },
+      { id: 4, name: 'Apollo', avatar: 'avatar_4.jpg', lastMessage: '', lastMessageDate: '2023-04-14T09:15:00', messages: [] },
+      { id: 5, name: 'Orazio', avatar: 'avatar_5.jpg', lastMessage: '', lastMessageDate: '2023-04-13T18:30:00', messages: [] },
+    ],
+      filteredContacts: [],
+      search: '',
+      activeContact: { name: '', messages: [] },
+      activeContactColor: '#4caf50',
+      inputMessage: '',
+    };
+  },
+  methods: {
+    
+    filterContacts() {
+      this.filteredContacts = this.contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(this.search.toLowerCase())
+      );
+    },
+    setActiveContact(contact) {
+      this.activeContact = contact;
+      this.messages = contact.messages; // Carica i messaggi del contatto
+    
+      
+    },
+  // Invia un messaggio e genera una risposta automatica dopo 1 secondo
+  sendMessage() {
+    if (this.inputMessage.trim()) {
+      const message = { id: Date.now(), text: this.inputMessage, type: 'sent' };
+      this.activeContact.messages.push(message);
+      this.activeContact.lastMessageDate = dt.now().toISO();
+      this.inputMessage = '';
+
+      setTimeout(() => {
+        const reply = { id: Date.now() + 1, text: 'Ok', type: 'received' };
+        this.activeContact.messages.push(reply);
+        this.activeContact.lastMessage = reply.text;
+       
+        //dt.now().toISO() CONVERTEl'oggetto DateTime corrente, rappresentante la data e l'ora correnti, in una stringa ISO . Questa stringa viene poi utilizzata per aggiornare la proprietà lastMessageDate del contatto attivo, permettendo di visualizzare l'orario dell'ultimo messaggio inviato o ricevuto
+
+        this.activeContact.lastMessageDate = dt.now().toISO();
+      }, 1000);
+
+    }
+  },
+  
+  formatDate(date) {
+    return dt.fromISO(date).setLocale('it').toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS);
+  },
+
+  
+
+
+  },
+  mounted() {
+    this.filteredContacts = this.contacts;
+  
+  },
+}).mount("#app");
